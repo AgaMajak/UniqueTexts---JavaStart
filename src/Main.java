9f9ce0a8df32dd0ec2da0ae863a0b8f76e908442
Agnieszka Majak <agamajak@wp.pl>
2020-05-01 01:40:04 +0200

    added specifiers

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,7 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
 
-        String textTemp;
-        String textA;
-        String textB;
+        TextCompare.compareTextsFromUser();
 
-        Scanner scan = new Scanner(System.in);
-        System.out.println("START:");
-        textA = scan.nextLine();
-        textB = scan.nextLine();
-        if (textA.equals(textB)) {
-            System.out.println("KONIEC");
-        } else
-            for (int i = 1; i > 0; i++) {
-                textTemp = scan.nextLine();
-                if (textTemp.equals(textB)) {
-                    System.out.println("KONIEC");
-                    break;
-                } else
-                    textB = textTemp;
-            }
     }
 }
--- src/TextCompare.java
+++ src/TextCompare.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
+
 public class TextCompare {
+
+    public static void compareTextsFromUser() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("START:");
+        String textA = scan.nextLine();
+        String textB = scan.nextLine();
+        if (textA.equals(textB)) {
+            System.out.println("KONIEC");
+        } else
+            for (int i = 1; i > 0; i++) {
+                String textTemp = scan.nextLine();
+                if (textTemp.equals(textB)) {
+                    System.out.println("KONIEC");
+                    break;
+                } else
+                    textB = textTemp;
+            }
+    }
 }
